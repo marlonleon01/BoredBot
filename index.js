32eba1d42961a45100f296ce4f4e93b025c4f3c9
Marlon Leon <ghostveene@gmail.com>
2023-02-08 10:46:14 -0500

    btn fetching data from api and displaying inside h4 element

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const activityName = document.getElementById("activity-name")
+const activityBtn = document.getElementById("new-idea-btn")
 
-fetch("https://apis.scrimba.com/bored/api/activity")
-    .then(respone => respone.json())
-    .then(data => activityName.textContent = data.activity)
+activityBtn.addEventListener("click", () => {
+    fetch("https://apis.scrimba.com/bored/api/activity")
+        .then(response => response.json())
+        .then(data => activityName.textContent = data.activity)
+})
+
+
