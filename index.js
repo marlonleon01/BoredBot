c326abd086d869c87298b1e2671727b1bb1de6eb
Marlon Leon <ghostveene@gmail.com>
2023-02-08 10:52:58 -0500

    added style when btn is clicked

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const activityName = document.getElementById("activity-name")
 const activityBtn = document.getElementById("new-idea-btn")
+const title = document.getElementById("title")
 
 activityBtn.addEventListener("click", () => {
     fetch("https://apis.scrimba.com/bored/api/activity")
         .then(response => response.json())
-        .then(data => activityName.textContent = data.activity)
+        .then(data => {
+            activityName.textContent = data.activity
+            title.textContent = "🦾 HappyBot🦿"
+            document.body.classList.add("fun")
+        })
 })
 
 
